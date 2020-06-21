def982770d63a497eb37da021072daa775300554
pengd-ksu <pengd@ksu.edu>
2020-06-21 00:25:49 -0500

    First commit for SICP

--- ch3.rkt
+++ ch3.rkt
@@ -1,1 +1,17 @@
 #lang sicp
+
+(define balance 100)
+
+(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      ("Insufficient funds")))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
