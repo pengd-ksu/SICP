c9115d37e113209f36c01d62cf064abf8db3ea16
pengd-ksu <pengd@ksu.edu>
2022-07-26 23:04:56 -0400

    Update on exerciese 2.29 in chp2

--- SICP_Ch2/ex2.28.rkt
+++ SICP_Ch2/ex2.28.rkt
@@ -1,33 +1,14 @@
 #lang sicp
-; Exercise 2.27
+; Exercise 2.28
+(define (fringe tree)
+  (cond ((null? tree) nil)
+        ((pair? tree) (append (fringe (car tree))
+                              (fringe (cdr tree))))
+        (else (list tree))))
 
-(define (reverse items)
-  (define (reverse-iter ls result)
-    (if (null? ls)
-        result
-        (reverse-iter (cdr ls) (append (list (car ls)) result))))
-  (reverse-iter items nil))
+(define x1 (list (list 1 2) (list 3 4)))
+(define x2 (list 1 4 9 16 25))
 
-(define (reverse-deep items)
-  (define (reverse-iter ls result)
-    (if (null? ls)
-        result
-        (if (pair? (car ls))
-            (reverse-iter (cdr ls) (cons (reverse-deep (car ls)) result))
-            (reverse-iter (cdr ls) (append (list (car ls)) result)))))
-  (reverse-iter items nil))
-
-(define (reverse-deep-v2 items)
-  (cond ((null? items) nil)
-        ((not (pair? items)) items)
-        (else (append (reverse-deep-v2 (cdr items))
-                      (list (reverse-deep-v2 (car items)))))))
-
-(reverse (list 1 4 9 16 25))
-(reverse (list (list 1 2) (list 3 4)))
-
-(reverse-deep (list 1 4 9 16 25))
-(reverse-deep (list (list 1 2) (list 3 4)))
-
-(reverse-deep-v2 (list 1 4 9 16 25))
-(reverse-deep-v2 (list (list 1 2) (list 3 4)))
+(fringe x1)
+(fringe x2)
+(fringe (list x1 x1))
---
+++ SICP_Ch2/ex2.29.rkt
@@ -0,0 +1,54 @@
+#lang sicp
+; Exercise 2.29
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cadr branch))
+
+(define (total-weight-branch branch)
+  (let ((w (branch-structure branch)))
+    (if (pair? w)
+        (total-weight-branch w)
+        w)))
+
+(define (total-weight-mobile mobile)
+  (+ (total-weight-branch (left-branch mobile))
+     (total-weight-branch (right-branch mobile))))
+
+; mutural recursion
+(define (balanced-branch? branch)
+  (let ((b (branch-structure branch)))
+    (if (pair? b)
+        (balanced-mobile? b)
+        #t)))
+
+(define (balanced-mobile? mobile)
+  (let ((left (left-branch mobile))
+        (right (right-branch mobile)))
+    (and (balanced-branch? left)
+         (balanced-branch? right)
+         (= (* (total-weight-branch left) (branch-length left))
+            (* (total-weight-branch right) (branch-length right))))))
+
+(define a (make-branch 5 3))
+(define b (make-mobile a (make-branch 1 (make-mobile a a))))
+(define c (make-mobile (make-branch 1 (make-mobile a a)) (make-branch 1 (make-mobile a a))))
+
+(total-weight-mobile b)
+(total-weight-mobile c)
+(balanced-mobile? b)
+(balanced-mobile? c)
