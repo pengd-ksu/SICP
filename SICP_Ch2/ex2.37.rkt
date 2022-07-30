cdc30fa8a3db8969524d181383183945c0aa556c
pengd-ksu <pengd@ksu.edu>
2022-07-30 11:55:23 -0400

    Update chp2

--- SICP_Ch2/ex2.37.rkt
+++ SICP_Ch2/ex2.37.rkt
@@ -1,5 +1,6 @@
 #lang sicp
-; Exercise 2.36
+; Exercise 2.37
+
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
@@ -11,3 +12,25 @@
       nil
       (cons (accumulate op init (map car seqs))
             (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product x v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
+
+(define m (list (list 1 2 3 4)
+                (list 4 5 6 6)
+                (list 6 7 8 9)))
+(define v (list 1 2 3 4))
+(matrix-*-vector m v)
+
+(transpose m)
+(matrix-*-matrix m (transpose m))
