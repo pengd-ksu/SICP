e02b87f4cabe2195073461699555e04ec023a051
pengd-ksu <pengd@ksu.edu>
2022-08-20 17:47:56 -0400

    Update for ch2

--- SICP_Ch2/ex2.55.rkt
+++ SICP_Ch2/ex2.55.rkt
@@ -5,3 +5,6 @@
 ;From footnote 34: the quotation mark is just a single-character
 ;abbreviation for wrapping the next complete expression with
 ;quote to form (quote ⟨expression⟩). Therefore, ' == quote
+
+(car (quote (quote abracadabra)))
+(car '(quote abracadabra))
